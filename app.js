cedc62e1c3ead720bdebd685cb5f61f245d9fa4e
BafS <fabacrans@gmail.com>
2017-02-26 00:55:02 +0100

    Add eslint, add link to edit a hurl, update some details

--- app.js
+++ app.js
@@ -1,53 +1,86 @@
-function getContentFromUrl() {
-    var b64Url = window.location.hash
-        .replace('#edit:', '#')
-        .substr(1)
-        .replace(/_/g, '/')
-    var binaryString = atob(b64Url)
-    if (binaryString) {
-        return pako.inflate(binaryString, { to: 'string' })
-    }
-    return '';
+function getUrlB64Content () {
+  return window.location.hash
+    .replace('#edit:', '#')
+    .substr(1)
+    .replace(/_/g, '/')
 }
 
-function hashChanged() {
-    var content = document.querySelectorAll('.content')[0]
-
-    marked.setOptions({
-        renderer: new marked.Renderer(),
-        gfm: true,
-        tables: true
-    });
-
-    content.innerHTML = marked(getContentFromUrl())
+function getUrlContent () {
+  var binaryString = atob(getUrlB64Content())
+  if (binaryString) {
+    return pako.inflate(binaryString, {to: 'string'})
+  }
+  return ''
 }
+
+function loadEditor () {
+  var submitEl = document.querySelectorAll('#submit')[0]
+  var editorEl = document.querySelectorAll('#editor')[0]
+
+  editorPanelEl.style.display = 'block'
+
+  if (window.location.hash.substr(0, 6) === '#edit:') {
+    editorEl.value = getUrlContent()
+  }
+
+  submitEl.addEventListener('click', function () {
+    var binaryString = pako.deflate(editorEl.value, {to: 'string'})
+
+    var b64 = window.btoa(binaryString)
+
+    var fullUrl = window.location.origin + '#' + b64.replace(/\//g, '_')
+
+    window.location = fullUrl
+  })
+}
+
+function hideEditor () {
+  editorPanelEl.style.display = 'none'
+}
+
+function loadContent () {
+  contentPanelEl.style.display = 'block'
+  var content = document.querySelectorAll('.content')[0]
+
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: true
+  })
+
+  content.innerHTML = marked(getUrlContent())
+
+  var editEl = document.querySelectorAll('.edit-link')[0]
+  editEl.addEventListener('click', function (e) {
+    e.preventDefault()
+    var fullUrl = window.location.origin + '#edit:' + getUrlB64Content().replace(/\//g, '_')
+
+    window.location = fullUrl
+  })
+}
+
+function hideContent () {
+  contentPanelEl.style.display = 'none'
+}
+
+function hashChanged () {
+  if (window.location.hash.length < 2 || window.location.hash.substr(0, 6) === '#edit:') {
+    hideContent()
+    loadEditor()
+  } else {
+    loadContent()
+    hideEditor()
+  }
+}
+
+var editorPanelEl = document.querySelectorAll('.editor-panel')[0]
+var contentPanelEl = document.querySelectorAll('.content-panel')[0]
 
 if (window.location.hash) {
-    hashChanged()
+  hashChanged()
+} else {
+  hideContent()
+  loadEditor()
 }
 
 window.onhashchange = hashChanged
-
-if (!window.location.hash || window.location.hash.substr(0, 6) === '#edit:') {
-    var editorPanelEl = document.querySelectorAll('.editor-panel')[0];
-    var submitEl = document.querySelectorAll('#submit')[0];
-    var editorEl = document.querySelectorAll('#editor')[0];
-    var resultUrlEl = document.querySelectorAll('#url-result')[0];
-
-    editorPanelEl.style.display = 'block'
-
-    if (window.location.hash.substr(0, 6) === '#edit:') {
-        editorEl.value = getContentFromUrl()
-    }
-
-    submitEl.addEventListener('click', function (e) {
-        var binaryString = pako.deflate(editorEl.value, { to: 'string' })
-
-        var b64 = window.btoa(binaryString)
-
-        var fullUrl = window.location.origin + '#' + b64.replace(/\//g, '_')
-
-        window.location.href = fullUrl
-        window.location.reload()
-    })
-}
