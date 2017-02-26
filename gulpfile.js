aaa239f86a384f6a6cbd7f833858541d86e4f198
BafS <fabacrans@gmail.com>
2017-02-26 01:02:06 +0100

    Add javascript build

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ const concat = require('gulp-concat')
 gulp.task('script', () => {
   return gulp.src([
     'node_modules/pako/dist/pako.min.js',
-    'node_modules/marked/marked.min.js'
-    // 'app.js'
+    'node_modules/marked/marked.min.js',
+    'app.js'
   ])
   .pipe(concat('hurl.js'))
   .pipe(gulp.dest('build'))
